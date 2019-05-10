278100d4a930a76a5d3ae8cdce4c7938f988ad65
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-05-10 17:34:08 -0500

    bootstrap_added

---
+++ assetts/java/game.js
@@ -0,0 +1,10 @@
+    var wordList = ["Shostakovitch", "Tchaikovsky", "Rachmoninoff", "Prokofiev", "Rimsky-Korsakov", "Mussorgsky"];
+
+    document.onkeyup = function() {
+        var userguess = string.fromCharCode (event.keyCode).toLowerCase();
+    console.log()
+    }
+
+    // var computerChoice = options[Math.floor(Math.random()*wordList.length)];
+    
+
