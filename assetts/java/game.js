0357a69085084d88c814c7235bbf2bc293fa7328
acepela5 <48183168+acepela5@users.noreply.github.com>
2019-05-10 19:47:49 -0500

    divs added

--- assetts/java/game.js
+++ assetts/java/game.js
@@ -1,10 +1,16 @@
-    var wordList = ["Shostakovitch", "Tchaikovsky", "Rachmoninoff", "Prokofiev", "Rimsky-Korsakov", "Mussorgsky"];
+    var wordList = [ "SHOSTAKOVITCH", "TCHAIKOVSKY", "RACHMONINOFF", "PROKOFIEV", "RIMSKYKORSAKOV", "MUSSORGSKY"];
 
+    // code referenced from Rock, Paper, Scissors solved video
+    // records keys pressed and  makes them uppercase
     document.onkeyup = function() {
-        var userguess = string.fromCharCode (event.keyCode).toLowerCase();
-    console.log()
+        var userguess = String.fromCharCode(event.keyCode).toUpperCase();
+    console.log(userguess);
     }
+//math.random chooses a random number between 0 and 1, is multiplied by the number of words in the array, and is rounded to the lowest whole number with Math.Floor 
+    var computerRandIndex = Math.floor(Math.random()*wordList.length);
+// computer number chose is aligned with word list 
+    var computerPickWord = wordList[computerRandIndex]; 
 
-    // var computerChoice = options[Math.floor(Math.random()*wordList.length)];
-    
+    console.log(computerPickWord);
 
+var numberofGuessCount = 9;
